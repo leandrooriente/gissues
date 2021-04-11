8866cb4421286682c0bebfa0113e091485188237
leandrooriente <leandro.oriente@dazn.com>
2021-04-11 16:44:03 +0200

    Add Header tests

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MemoryRouter, Switch, Route } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Header } from "./Header";
+
+const mockHistoryPush = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+describe("Header", () => {
+  beforeEach(() => {
+    mockHistoryPush.mockClear();
+  });
+
+  it("Should redirect", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Header />
+      </MemoryRouter>
+    );
+
+    userEvent.click(screen.getByLabelText("Organization"));
+    document.activeElement &&
+      userEvent.type(document.activeElement, "facebook");
+
+    userEvent.click(screen.getByLabelText("Repository"));
+    document.activeElement && userEvent.type(document.activeElement, "react");
+
+    userEvent.click(screen.getByRole("button", { name: "Search" }));
+
+    expect(mockHistoryPush).toHaveBeenCalledWith("/facebook/react/1");
+  });
+
+  it("Should initiate with the route state", () => {
+    render(
+      <MemoryRouter initialEntries={["/facebook/react/1"]}>
+        <Switch>
+          <Route path="/:organization/:repository/:page" component={Header} />
+        </Switch>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByDisplayValue("facebook")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("react")).toBeInTheDocument();
+  });
+});
